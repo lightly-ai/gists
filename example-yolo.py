0abfa7cdecd4b6adeba3a7b260b4ba4a98ab5ebf
Kondrat Shmoylov <kondrat.shmoylov@gmail.com>
2025-02-04 10:25:58 +0100

    feat: add yolo8 example

---
+++ example-yolo.py
@@ -0,0 +1,17 @@
+import os
+
+from lightly_purple import DatasetLoader
+
+# Create a DatasetLoader instance
+loader = DatasetLoader()
+
+# Define the path to the dataset (folder containing data.yaml)
+dataset_path = os.getenv("DATASET_PATH")
+
+# Load YOLO dataset using data.yaml path
+yolo_loader, dataset_id = loader.from_yolo(
+    f"{dataset_path}/dataset.yaml",
+    input_split="train",
+)
+
+loader.launch()
