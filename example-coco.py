5fba039f4fe26e28a6844f5cb44e562fb295dc07
Horatiu Almasan <horatiu@lightly.ai>
2025-03-18 14:05:25 +0200

    Update coco example to work with the dataset from roboflow.

--- example-coco.py
+++ example-coco.py
@@ -9,13 +9,13 @@ loader = DatasetLoader()
 dataset_path = os.getenv("DATASET_PATH")
 
 # We load the COCO dataset using the defined DATASET_PATH
-# We point to annotations.json and the input image folder.
+# We point to the annotations json file and the input image folder.
 # The image folder can be an absolute path or a relative path to the annotations.json file.
 
 # Defined dataset is processed here to be available for the UI application and further operations.
 coco_loader, dataset_id = loader.from_coco(
-    f"{dataset_path}/annotations.json",
-    input_images_folder="image_folder"
+    f"{dataset_path}/_annotations.json",
+    input_images_folder="./"
 )
 
 loader.launch()
