e95008769d222b087e3778b60b87eb45601a6649
Kondrat Shmoylov <kondrat.shmoylov@gmail.com>
2025-04-16 11:38:17 +0200

    Update example-coco.py

--- example-coco.py
+++ example-coco.py
@@ -7,8 +7,8 @@ loader = DatasetLoader()
 # We point to the annotations json file and the input images folder.
 # Defined dataset is processed here to be available for the UI application.
 loader.from_coco_instance_segmentations(
-    annotations_json_path="dataset/_annotations.coco.json",
-    input_images_folder="dataset/train",
+    annotations_json_path=".datasets/coco-128/test/_annotations.coco.json",
+    input_images_folder=".datasets/coco-128/test",
 )
 
 # We start the UI application on port 8001
